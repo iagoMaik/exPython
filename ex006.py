3f4f764f918dbf6b61f520586f658f714fcac11c
IagoMaik <iagomaik273@gmail.com>
2022-03-16 00:28:58 -0300

    Create ex006.py
    Exercício 006 = Python 3 = Mundo 1 = Curso em Vídeo

---
+++ ex006.py
@@ -0,0 +1,5 @@
+num = int(input('Digite um número: '))
+dobro = num * 2
+triplo = num * 3
+raizQ = num ** (1/2)
+print('- O dobro de {} é {} \n- O triplo de {} é {} \n- A raíz quadrada de {} é {}'.format(num, dobro, num, triplo, num, raizQ))
