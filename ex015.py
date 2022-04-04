9b8ef2e97e1d06d247f9520a6c4ec61e2807df67
IagoMaik <iagomaik273@gmail.com>
2022-04-04 18:02:58 -0300

    Create ex015.py
    Exercício 015 = Python 3 = Mundo 1 = Curso em Vídeo

---
+++ ex015.py
@@ -0,0 +1,5 @@
+kmP = float(input('Kilômetros percorridos pelo cliente: '))
+diasAluguel = float(input('Total de dias que o veículo foi alugado: '))
+precoCarro = 60 * diasAluguel
+precokmP = 0.15 *kmP
+print('O preço total à ser pago pelo clinte é de: R$', precoCarro + precokmP)
