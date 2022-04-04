561c03bea74049ab9de0fc56ce47f6c17aa9ebeb
IagoMaik <iagomaik273@gmail.com>
2022-04-04 18:01:53 -0300

    Update ex008.py

--- ex008.py
+++ ex008.py
@@ -1,6 +1,15 @@
 m = float(input('Digite um tamanho (em metros): '))
-cm = m * (10 ** 2)
-mm = m * (10 ** 3)
+dm = m * 10
+cm = m * 100
+mm = m * 1000
+dam = m / 10
+hm = m / 100
+km = m / 1000
 print('convertendo...')
+print('[dm] =', dm)
 print('[cm] =', cm)
 print('[mm] =', mm)
+print('[dam] =', dam)
+print('[hm] =', hm)
+print('[km] =', km)
+
